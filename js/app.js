dd8f9343a044d203c0b7ce68e0ae3d739afb0f81
Elizandra Pruner <elizandramachado87@hotmail.com>
2019-01-05 14:05:03 -0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
  * Create a list that holds all of your cards
  */
 
+const arrayimg = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-anchor', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-leaf', 'fa-bomb', 'fa-bolt', 'fa-bicycle', 'fa-paper-plane-o', 'fa-cube'];
+const allStars = document.getElementsByClassName('fa-star'); /*Insere na array todos os elementos com a class fa-star/todas as estrelas*/
+let open = [];
+let deck = document.querySelector('.deck'); 
+let initGame = 0;
+let lock = false;
+let moves = 0;
+let gameInterval;
+let cardsMatcheds = 0;
 
 /*
  * Display the cards on the page
@@ -25,7 +34,151 @@ function shuffle(array) {
     return array;
 }
 
+function startG(){
+	cardsMatcheds = 0;
+	document.querySelector('.modal').classList.remove('modal-show'); /*Esconde o modal no reset*/
+	for (let i = 0; i < allStars.length; i++){ /*Resetando o valor das estrelas*/
+		allStars[i].classList.remove('hide-star');
+	}
+	shuffle(arrayimg);
+	document.getElementsByClassName('deck')[0].innerHTML = ''; /*Recebe o elemento ul buscando pela class e armazena em uma array, como é uma array precisa informar a posição por isso do [0] depois do ('deck') o innerHTML = '' limpa o conteudo de ul*/
+	moves = 0;
+	document.querySelector('.moves').innerHTML = moves; /*Para limpar na tela do html também não só zerar o moves*/
+	document.querySelector('#movesEnd').innerHTML = moves; /*Limpar o moves no modal*/
+	open = [];
+	initGame = 0; /*Para o timer poder ser inicializado*/
+	clearInterval(gameInterval); /*Reseta o timer para 00:00*/
+	document.querySelector('.displayTime').innerHTML = '00:00'; /*Limpa o tempo na tela do html*/
+	document.querySelector('#timeEnd').innerHTML = '00:00';
+	for (let i = 0; i < 16; i++){
+		let liElement = document.createElement('li');
+		liElement.classList.add('card');
+		let iElement = document.createElement('i');
+		iElement.classList.add('fa'); 		
+		iElement.classList.add(arrayimg[i]); 
+		liElement.append(iElement);
+		liElement.addEventListener('click', clicked);	
+		deck.append(liElement);
+	}
+	
+}
+	
+startG();
+restart();
+playAgain();
+NotPlayAgain();
 
+function hideStars(){
+	if (moves === 16) { 
+		allStars[0].classList.add('hide-star'); /*Esconde a primeira estrela do score-panel*/
+		allStars[3].classList.add('hide-star'); /*Esconde a primeira estrela do modal*/
+	} else if (moves === 20) { 
+		allStars[1].classList.add('hide-star'); /*Esconde a segunda estrela do score-panel*/
+		allStars[4].classList.add('hide-star'); /*Esconde a segunda estrela do modal*/
+	}
+}
+
+function count_moves(){
+	moves += 1;
+	document.querySelector('.moves').innerHTML = moves;
+	document.querySelector('#movesEnd').innerHTML = moves;
+	if (20 >= moves > 0) { 
+		hideStars();
+	}
+}
+
+
+function clicked(){
+
+	if (this.classList.contains('open') && this.classList.contains('show')){ 
+		return true;
+	}
+
+	if (initGame === 0) { 
+		timer();
+		initGame = 1;
+	}
+
+	if (lock == false && open.length < 2){
+		this.classList.toggle('show');
+		this.classList.toggle('open');
+		open.push(this);
+	}
+
+	if (lock == false && open.length === 2) {
+		count_moves();
+		lock = true;
+			if (open[0].firstChild.classList[1] === open[1].firstChild.classList[1]) {
+			open[0].classList.toggle('match');
+			open[1].classList.toggle('match');
+			open = [];	
+			console.log('match', lock)
+			setTimeout(function(){
+				lock = false;
+			}, 1000)
+			cardsMatcheds += 1; 
+			if (cardsMatcheds === 8){
+				document.querySelector('.modal').classList.add('modal-show');
+				clearInterval(gameInterval); 
+
+				}
+		} else {
+			open[0].classList.toggle('unMatch');
+			open[1].classList.toggle('unMatch');
+			setTimeout(function(){
+				open[0].classList.toggle('show');
+				open[1].classList.toggle('show');
+				open[0].classList.toggle('open');
+				open[1].classList.toggle('open');
+				open[0].classList.toggle('unMatch');
+				open[1].classList.toggle('unMatch');
+				lock = false;
+				open = [];	
+				}, 1000) 
+		}
+	}
+}
+
+
+function restart(){
+	let reset = document.getElementById('repeat'); 
+	reset.addEventListener('click', function(){
+		startG();		
+	})
+}
+
+ function timer(){
+     let minutes = 0;
+     let seconds = 0;
+     gameInterval = setInterval(function () {
+         seconds = parseInt(seconds, 10) + 1;
+         minutes = parseInt(minutes, 10);
+         if (seconds >= 60) {
+             minutes += 1;
+             seconds = 0;
+         }
+         seconds = seconds < 10 ? "0" + seconds : seconds;
+         minutes = minutes < 10 ? "0" + minutes : minutes;
+         $('.displayTime').html(minutes + ":" + seconds);
+         $('#timeEnd').text($('.displayTime').text());
+     }, 1000);
+ }
+
+ function playAgain(){
+ 	buttonYes = document.getElementById('yes');
+ 	buttonYes.addEventListener('click', function(){
+	startG();
+ })
+}
+
+function NotPlayAgain(){
+	buttonNot = document.getElementById('not');
+	buttonNot.addEventListener('click', function(){
+	document.querySelector('.modal').classList.remove('modal-show');
+	})
+}
+
+	
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
